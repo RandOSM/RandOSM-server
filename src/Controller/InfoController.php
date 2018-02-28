2846ac22b025fb4a1eeaf3e8d7cd004a825f5ec6
Théo Bougé <theo.bouge@gmail.com>
2018-02-28 14:12:12 +0100

    Add info_osm page

---
+++ src/Controller/InfoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use \Twig_Environment;
+
+class InfoController
+{
+
+    private $twig;
+
+    public function __construct(Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function osm()
+    {
+        return new Response($this->twig->render('info/osm.html.twig'));
+    }
+
+}
